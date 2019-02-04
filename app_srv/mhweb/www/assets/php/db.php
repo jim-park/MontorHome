6213aab511b02df491609ab4abedb2f5b5a4a601
lnlgit <jim@linuxnetworks.co.uk>
2019-02-04 13:54:49 +0000

    Redundant file removed.

--- app_srv/mhweb/www/assets/php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-        if( !($db = mysql_connect('localhost', 'root', '0HFYad47')))
-            die('Failed to connect to Server' - #'.mysql_errno().': '.mysql_error();
-        if( !mysql_select_db( 'mh_data' ) )
-            die( 'Connected to Server, but Failed to Connect to Database - #'.mysql_errno().': '.mysql_error();
-
-        >
