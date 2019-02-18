787f8985730ea4d0b007b8299da613960561c458
lnlgit <jim@linuxnetworks.co.uk>
2019-02-18 21:26:42 +0000

    Added new tracer driver methods and new driver tests. Tidied up code and comments.

--- raspi/test/mock_tracerbn.py
+++ raspi/test/mock_tracerbn.py
@@ -8,34 +8,34 @@ import os
 import threading
 import minimalmodbus
 from minimalmodbus import MODE_RTU
-from random import randrange
+from random import randrange, randint
 
 
 def get_two_bytes(data):
-    """Return a list of the two lsbytes from an int."""
+    """ Return a list of the two lsbytes from an int. """
     return (data & 0xFF), (data >> 8) & 0xFF
 
 
 def get_four_bytes(data):
-    """Return a list of the four lsbytes from an int."""
+    """ Return a list of the four lsbytes from an int. """
     return (data & 0xFF), (data >> 8) & 0xFF, (data >> 16) & 0xFF, (data >> 24) & 0xFF
 
 
 def get_two_byte_bytearray(data):
-    """Return a list of the two lsbytes from an int, plus their count at element 0."""
+    """ Return a list of the two lsbytes from an int, plus their count at element 0. """
     byte0, byte1 = get_two_bytes(data)
     return bytearray([2, byte1, byte0])
 
 
 def get_four_byte_bytearray(data):
-    """Return a list of the four lsbytes from an int (reversed), plus their count at element 0."""
+    """ Return a list of the four lsbytes from an int (reversed), plus their count at element 0. """
     byte0, byte1, byte2, byte3 = get_four_bytes(data)
     # Note the byte order.
     return bytearray([4, byte1, byte0, byte3, byte2])
 
 
 class RequestCodes:
-    """Raw modbus request codes."""
+    """ Raw modbus request codes. """
     device_info = bytearray([0x01, 0x2b, 0x0e, 0x01, 0x00, 0x70, 0x77])
     batt_voltage = bytearray([0x01, 0x04, 0x33, 0x1A, 0x00, 0x01, 0x1F, 0x49])
     batt_current = bytearray([0x01, 0x04, 0x33, 0x1B, 0x00, 0x02, 0x0E, 0x88])
@@ -44,14 +44,21 @@ class RequestCodes:
     batt_soc = bytearray([0x01, 0x04, 0x31, 0x1A, 0x00, 0x01, 0x1E, 0xF1])
     batt_voltage_max_today = bytearray([0x01, 0x04, 0x33, 0x02, 0x00, 0x01, 0x9F, 0x4E])
     batt_voltage_min_today = bytearray([0x01, 0x04, 0x33, 0x03, 0x00, 0x01, 0xCE, 0x8E])
+    batt_status = bytearray([0x01, 0x04, 0x32, 0x00, 0x00, 0x01, 0x3F, 0x72])
+    batt_rated_voltage = bytearray([0x01, 0x04, 0x30, 0x04, 0x00, 0x01, 0x7F, 0x0B])
+    charging_equip_status = bytearray([0x01, 0x04, 0x32, 0x01, 0x00, 0x01, 0x6E, 0xB2])
+    load_current = bytearray([0x01, 0x04, 0x31, 0x0D, 0x00, 0x01, 0xAE, 0xF5])
+    load_voltage = bytearray([0x01, 0x04, 0x31, 0x0C, 0x00, 0x01, 0xFF, 0x35])
+    load_power = bytearray([0x01, 0x04, 0x31, 0x0E, 0x00, 0x02, 0x1E, 0xF4])
 
     def __init__(self): pass
 
 
 class MockTracerBN(threading.Thread):
-    """This acts as a poor mans TracerBN emulator. It runs in a separate thread,
-        when it receives a modbus request, it services the request with sensible
-        values, then immediately terminates."""
+    """ This acts as a poor mans TracerBN emulator. It runs in a seperate thread.
+        When it receives a modbus request, it services the request with a sensible
+        response, then immediately terminates.
+    """
 
     def __init__(self, fd):
         threading.Thread.__init__(self)
@@ -59,7 +66,7 @@ class MockTracerBN(threading.Thread):
         print "MockTracer created"
 
     def run(self):
-        """Service one request and exit. No loops."""
+        """ Service one request and exit. No loops. """
 
         print "MockTracer running"
 
@@ -68,7 +75,6 @@ class MockTracerBN(threading.Thread):
 
         # Request for device information.
         if rx_request == RequestCodes.device_info:
-            print "Request for device information"
             # Return a string containing 'Tracer'.
             tx_string = "1234xyx+_&^$#Tracer#xyx+_&^$#4321"
             # Send string.
@@ -76,50 +82,111 @@ class MockTracerBN(threading.Thread):
 
         # Request for battery voltage.
         elif rx_request == RequestCodes.batt_voltage:
-            print "Request for batt voltage"
             # Voltage returned is x100. 2 bytes. 10.00V to 15.00V.
             self.send(get_two_byte_bytearray(randrange(1000, 1500)))
 
         # Request for battery current.
         elif rx_request == RequestCodes.batt_current:
-            print "Request for batt current"
             # Current returned is x100. 4 bytes. -30.00A to 30.00A.
             self.send(get_four_byte_bytearray(randrange(-3000, 3000)))
 
         # Request for battery power.
         elif rx_request == RequestCodes.batt_power:
-            print "Request for batt power"
             # Power returned is x100. 4 bytes. 0.00W to 150.00W.
             self.send(get_four_byte_bytearray(randrange(0, 15000)))
 
         # Request for battery temperature.
         elif rx_request == RequestCodes.batt_temp:
-            print "Request for batt temperature"
             # Temperature returned is x100. 2 bytes -10.00 to 150.00 degC.
             self.send(get_two_byte_bytearray(randrange(-1000, 15000)))
 
         # Request for battery soc.
         elif rx_request == RequestCodes.batt_soc:
-            print "Request for batt SOC"
             # SOC is returned as a percentage. 2 bytes.
             self.send(get_two_byte_bytearray(randrange(0, 100)))
 
         # Request for battery max voltage today.
         elif rx_request == RequestCodes.batt_voltage_max_today:
-            print "Request for batt max voltage today"
             # Max voltage today returned is x100. 2 bytes. 10.00V to 20.00V.
             self.send(get_two_byte_bytearray(randrange(1000, 2000)))
 
         # Request for battery min voltage today.
         elif rx_request == RequestCodes.batt_voltage_min_today:
-            print "Request for batt max voltage today"
             # Min voltage today returned is x100. 2 bytes. 10.00V to 20.00V.
             self.send(get_two_byte_bytearray(randrange(1000, 2000)))
 
+        # Request for rated battery voltage
+        elif rx_request == RequestCodes.batt_rated_voltage:
+            # Rated voltage returned 'should' be one of 9 discrete V values x100. 2 bytes. 12V to 240V.
+            self.send(get_two_byte_bytearray(randrange(1200, 24000)))
+
+        # Request for load current
+        elif rx_request == RequestCodes.load_current:
+            # Load current returned is x100. 2 bytes. 0.00A to 30.00A.
+            self.send(get_two_byte_bytearray(randrange(0, 3000)))
+
+        # Request for load voltage
+        elif rx_request == RequestCodes.load_voltage:
+            # Load current returned is x100. 2 bytes. 10.00V to 20.00V.
+            self.send(get_two_byte_bytearray(randrange(0, 2000)))
+
+        # Request for load power
+        elif rx_request == RequestCodes.load_power:
+            # Load power returned is x100. 4 bytes. 0.00W to 360.00W.
+            self.send(get_four_byte_bytearray(randrange(0, 36000)))
+
+        # Request for battery status.
+        elif rx_request == RequestCodes.batt_status:
+            # Build 2 bytes to return, bit by bit.
+            # Build response byte 0.
+            byte0_bits0_3 = randint(0, 3)           # bits 0-3: in range 0-3
+            byte0_bits4_7 = (randint(0, 2) << 4)    # bits 4-7: in range 0-2
+
+            # Build response byte 1.
+            byte1_bit8 = randint(0, 1)              # bit 8: boolean
+            byte1_bit15 = randint(0, 1)             # bit 15: boolean
+
+            # Make each byte
+            byte0 = byte0_bits0_3 | byte0_bits4_7
+            byte1 = byte1_bit8 | byte1_bit15
+            # print "byte0: 0x%x, byte1: 0x%x" % (byte0, byte1)
+
+            self.send(get_two_byte_bytearray(byte0 | (byte1 << 8)))
+
+        # Request for charging equipment status.
+        elif rx_request == RequestCodes.charging_equip_status:
+            def randbool():
+                return randint(0, 1)
+
+            # Build 2 bytes to return, bit by bit.
+            # Build response byte 0.
+            byte0_bit0 = randbool()                 # bit 0: boolean
+            byte0_bit1 = randbool() << 1            # bit 1: boolean
+            byte0_bits2_3 = randrange(0, 2) << 2    # bits 2-3: in range 0-2
+            byte0_bit4 = randbool() << 4            # bit 4: boolean
+            byte0_bit7 = randbool() << 7            # bit 7: boolean
+
+            # Build response byte 1.
+            byte1_bit0 = randbool()                 # bit 0: boolean
+            byte1_bit1 = randbool() << 1            # bit 1: boolean
+            byte1_bit2 = randbool() << 2            # bit 2: boolean
+            byte1_bit3 = randbool() << 3            # bit 3: boolean
+            byte1_bit4 = randbool() << 4            # bit 4: boolean
+            byte1_bit5 = randbool() << 5            # bit 5: boolean
+            byte1_bits6_7 = randrange(0, 3) << 6    # bits 6-7: in range 0-3
+
+            # Make each byte.
+            byte0 = byte0_bit0 | byte0_bit1 | byte0_bits2_3 | byte0_bit4 | byte0_bit7
+            byte1 = byte1_bit0 | byte1_bit1 | byte1_bit2 | byte1_bit3 | byte1_bit4 | byte1_bit5 | byte1_bits6_7
+            # print "byte0: 0x%x, byte1: 0x%x" % (byte0, byte1)
+
+            self.send(get_two_byte_bytearray(byte0 | (byte1 << 8)))
+
+        # Exit the run() method and terminate.
         print "MockTracer exiting"
 
     def send(self, tx_data):
-        """Write response data back to the driver"""
+        """ Write response data back to the driver. """
         # Allow minimalmodbus to put the final response together for us, adding the crc.
         resp = minimalmodbus._embedPayload(1, MODE_RTU, 4, '%s' % tx_data)
         # Send response.
--- raspi/test/test_tracerbn.py
+++ raspi/test/test_tracerbn.py
@@ -18,8 +18,15 @@ from mock_tracerbn import MockTracerBN
 
 
 class TestTracerBN(unittest.TestCase):
+    """ This is a test case to provide unit tests for the tracerbn driver.
+
+        This test case relies on the MockTracerBN class which
+        needs to imported from mock_tracer for it to function.
+
+    """
 
     def setUp(self):
+        """ Setup mock device, fds, and tranerbn driver. """
         # Get new pty master slave ends
         self.fd_m, self.fd_s = pty.openpty()
 
@@ -29,10 +36,11 @@ class TestTracerBN(unittest.TestCase):
         # Create a new mock tracer
         self.mock_tracer = MockTracerBN(self.fd_m)
 
-        # Get the tracer driver under test
+        # Get the tracerbn driver under test.
         self.driver = TracerBN(portname=self.fd_s_path)
 
     def tearDown(self):
+        """ Stop and delete mock device, close file descriptors. """
         # Join the thread if required.
         if self.mock_tracer.isAlive():
             self.mock_tracer.join(1.0)
@@ -45,19 +53,20 @@ class TestTracerBN(unittest.TestCase):
         del self.mock_tracer
 
     def test_switch_4_bytes(self):
+        """ Test switch_4_bytes() function. """
         a = 0x89AB0000
         b = 0x0000CDEF
         correct_result = (a >> 16) | (b << 16)
         self.assertEquals(switch_4_bytes(a | b), correct_result)
 
     def test_find_serial_port_exception(self):
-        # Call find_serial_port function without the mock Tracer running (present but no response).
+        """ Test find_serial_port() raises an exception if it can't find a device (present but no response). """
         self.assertRaises(Exception, find_serial_port, ports_list=[self.fd_s_path])
 
     def test_find_serial_port_success(self):
-        # Start the tracer
+        """ Test find_serial_port() returns the device path if it successfully finds a device. """
         self.mock_tracer.start()
-        # Call find_serial_port function with the mock Tracer running (we expect the correct response).
+
         returned_path = find_serial_port(ports_list=[self.fd_s_path])
         self.assertEqual(returned_path, self.fd_s_path)
 
@@ -65,47 +74,134 @@ class TestTracerBN(unittest.TestCase):
     # Test battery related methods
     #
     def test_get_batt_voltage_success(self):
-        # We expect a random voltage value in the range 10.00 < v < 15.00.
-        self.within_range(self.driver.get_batt_voltage, 10.0, 15.0)
+        """ Test get_batt_voltage() method returns a value between 10.0 and 15.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_voltage, 10.0, 15.0)
 
     def test_get_batt_current_success(self):
-        # We expect a random current value in the range 30.00 < A < 30.00.
-        self.within_range(self.driver.get_batt_current, -30.0, 30.0)
+        """ Test get_batt_current() method returns a value between -30.0 and 30.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_current, -30.0, 30.0)
 
     def test_get_batt_power_success(self):
-        # We expect a random power value in the range 0.00 < W < 150.00.
-        self.within_range(self.driver.get_batt_power, 0.0, 150.0)
+        """ Test get_batt_power() method returns a value between 0.0 and 150.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_power, 0.0, 150.0)
 
     def test_get_batt_temp_success(self):
-        """TracerBN.get_batt_temp"""
-        # We expect a random temperature value in the range -50 < degC < 150.00.
-        self.within_range(self.driver.get_batt_temp, -50.0, 150)
+        """ Test get_batt_temp() method returns a value between -50.0 and 150.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_temp, -50.0, 150.0)
 
     def test_get_batt_soc_success(self):
-        """TracerBN.get_batt_soc"""
-        # We expect a random percentage in the range 0 < % < 100.
-        self.within_range(self.driver.get_batt_soc, 0, 100)
+        """ Test get_batt_soc() method returns a value between 0.0 and 100.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_soc, 0, 100)
 
     def test_get_batt_voltage_max_today_success(self):
-        # We expect a random voltage in the range 10 < V < 20.
-        self.within_range(self.driver.get_batt_voltage_max_today, 10, 20)
+        """ Test get_batt_voltage_max_today() method returns a value between 10.0 and 20.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_voltage_max_today, 10.0, 20.0)
 
     def test_get_batt_voltage_min_today_success(self):
-        # We expect a random voltage in the range 10 < V < 20.
-        self.within_range(self.driver.get_batt_voltage_min_today, 10, 20)
+        """ Test get_batt_voltage_min_today() method returns a value between 10.0 and 20.0. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_voltage_min_today, 10.0, 20.0)
 
-    def within_range(self, funct, min_value, max_value):
-        """Generic function to test driver returns values within a given range"""
+    def test_get_batt_rated_voltage(self):
+        """ Test get_batt_rated_voltage() method returns a value between 12 and 240. """
+        self.assertFuncIsWithinRange(self.driver.get_batt_rated_voltage, 12, 240)
+
+    def test_get_batt_status_success(self):
+        """ Test get_batt_status() method returns an array with the expected results. """
 
         # Start the tracer.
         self.mock_tracer.start()
 
-        # Execute the function to fetch the result.
+        returned_array = self.driver.get_batt_status()
+
+        print returned_array
+        # Test element0 - batt status - is within range 0 to 4.
+        self.assertIsWithinRange(returned_array[0], 0, 4)
+
+        # Test element1 - batt temp status - is within range 0 to 2.
+        self.assertIsWithinRange(returned_array[1], 0, 2)
+
+        # Test element2 - BIR fault flag - is a boolean.
+        self.assertIsWithinRange(returned_array[2], 0, 1)
+
+        # Test element3 - wrong id for rated voltage flag - is a boolean.
+        self.assertIsWithinRange(returned_array[3], 0, 1)
+
+    def test_get_charging_equip_status_success(self):
+        """ Test get_charging_equip_status() method returns an array with the expected results. """
+
+        # Start the tracer.
+        self.mock_tracer.start()
+
+        returned_array = self.driver.get_charging_equip_status()
+
+        # Test element 0 - Charging Equipment Running - is a boolean.
+        self.assertIsWithinRange(returned_array[0], 0, 1)
+
+        # Test element 1 - Charging Equipment Status - is a boolean.
+        self.assertIsWithinRange(returned_array[1], 0, 1)
+
+        # Test element 2 - Charging Status - is within range 0-3.
+        self.assertIsWithinRange(returned_array[2], 0, 3)
+
+        # Test element 3 - PV Input is Short - is a boolean.
+        self.assertIsWithinRange(returned_array[3], 0, 1)
+
+        # Test element 4 - Load MOSFET is Short - is a boolean.
+        self.assertIsWithinRange(returned_array[4], 0, 1)
+
+        # Test element 5 - Load is Short - is a boolean.
+        self.assertIsWithinRange(returned_array[5], 0, 1)
+
+        # Test element 6 - Load is Over-Current - is a boolean.
+        self.assertIsWithinRange(returned_array[6], 0, 1)
+
+        # Test element 7 - Input is Over-Current - is a boolean.
+        self.assertIsWithinRange(returned_array[7], 0, 1)
+
+        # Test element 8 - Anti-Reverse MOSFET is short - is a boolean.
+        self.assertIsWithinRange(returned_array[8], 0, 1)
+
+        # Test element 9 - Charging, or Anti-Reverse MOSFET is short - is a boolean.
+        self.assertIsWithinRange(returned_array[9], 0, 1)
+
+        # Test element 10 - Charging MOSFET is short - is a boolean.
+        self.assertIsWithinRange(returned_array[10], 0, 1)
+
+        # Test element 11 - Input Voltage Status - is within range 0-3.
+        self.assertIsWithinRange(returned_array[11], 0, 3)
+
+    def test_get_load_current_success(self):
+        """ Test get_load_current() method returns a value between 0.0 and 30.0. """
+        self.assertFuncIsWithinRange(self.driver.get_load_current, 0.0, 30.0)
+
+    def test_get_load_voltage_success(self):
+        """ Test get_load_voltage() method returns a value between 10.0 and 20.0. """
+        self.assertFuncIsWithinRange(self.driver.get_load_voltage, 0.0, 20.0)
+
+    def test_get_load_power_success(self):
+        """ Test get_load_power() method returns a value between 0.0 and 360.0. """
+        self.assertFuncIsWithinRange(self.driver.get_load_power, 0.0, 360.0)
+
+    def assertFuncIsWithinRange(self, funct, min_value, max_value):
+        """ Assert method to test a driver method returns values within a given range. """
+
+        # Start the tracer.
+        self.mock_tracer.start()
+
+        # Execute the driver method to fetch the result.
         x = funct()
 
-        # Test the result is within the given range.
-        self.assertGreaterEqual(x, min_value)
-        self.assertLessEqual(x, max_value)
+        self.assertIsWithinRange(x, min_value, max_value)
+
+    def assertIsWithinRange(self, x, min_value, max_value):
+        """ Assert method to test x is within, or equal to the given values. """
+
+        if min_value <= max_value:
+            # Test the result is within the given range.
+            self.assertGreaterEqual(x, min_value)
+            self.assertLessEqual(x, max_value)
+        else:
+            raise("max and min value must be just that, not min=%s, max=%s." % (min_value, max_value))
 
 
 if __name__ == "__main__":
